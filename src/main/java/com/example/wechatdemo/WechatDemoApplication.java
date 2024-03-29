589b57375b303510f3f6bd9c61f2cdf0de1d7ad6
KuiyanZhao <zhao1360@purdue.edu>
2024-03-29 12:39:33 +0800

    test

--- src/main/java/com/example/wechatdemo/WechatDemoApplication.java
+++ src/main/java/com/example/wechatdemo/WechatDemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WechatDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WechatDemoApplication.class, args);
     }
